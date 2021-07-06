98fe158d0e1aa01cfe7d054c327b5e33f5a290e7
mpoiitis <marinospoet@gmail.com>
2021-07-06 11:12:16 +0300

    Add early stopping class

--- utils/utils.py
+++ utils/utils.py
@@ -542,3 +542,34 @@ def get_file_count(directory, str_to_search):
     files = [file for file in files if str_to_search in file]
     file_count = len(files)
     return file_count
+
+
+class EarlyStopping:
+    """
+    Early stopping to stop the training when the loss does not improve after
+    certain epochs.
+    """
+    def __init__(self, patience=5, min_delta=0):
+        """
+        :param patience: how many epochs to wait before stopping when loss is
+               not improving
+        :param min_delta: minimum difference between new loss and old loss for
+               new loss to be considered as an improvement
+        """
+        self.patience = patience
+        self.min_delta = min_delta
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+
+    def __call__(self, val_loss):
+        if not self.best_loss:
+            self.best_loss = val_loss
+        elif self.best_loss - val_loss > self.min_delta:
+            self.best_loss = val_loss
+        elif self.best_loss - val_loss < self.min_delta:
+            self.counter += 1
+            print(f"INFO: Early stopping counter {self.counter} of {self.patience}")
+            if self.counter >= self.patience:
+                print('INFO: Early stopping')
+                self.early_stop = True
